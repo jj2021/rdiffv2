2d3c38d701a0b427458416c5ca22874127432bc0
jj2021 <james.jenkins2@marist.edu>
2022-09-05 01:32:41 +0000

    Utilize new CircularArray
    Utilize the CircularArray struct in order to access the position vector
    with negative indicies.

--- src/rdiff.rs
+++ src/rdiff.rs
@@ -1,12 +1,10 @@
 // Myers diff algorithm implementation
 
-/*
-// This circular array implementation works!!! 
-struct CircArray {
+struct CircularArray {
   arr: Vec<i32>,
 }
 
-impl CircArray {
+impl CircularArray {
   fn new(size: usize) -> Self {
     Self {
       arr: vec![-1; size],
@@ -30,26 +28,6 @@ impl CircArray {
     }
   }
 }
-*/
-
-// get value by index of slice, including negative indicies
-fn get_value(record: &[i32], index: i32) -> i32 {
-    if index < 0 {
-      let real_index = record.len() as i32 + index;
-      return record[real_index as usize];
-    }
-    record[index as usize]
-}
-
-// set value by index of slice, including negative indicies
-fn set_value(record: &mut [i32], index: i32, value: i32) {
-    if index < 0 {
-      let real_index = record.len() as i32 + index;
-      record[real_index as usize] = value;
-    } else {
-      record[index as usize] = value;
-    }
-}
 
 // TODO: Implement Longest Common Subsequence method
 fn lcs(a: &str, b: &str) -> i32  {
@@ -66,10 +44,7 @@ fn lcs(a: &str, b: &str) -> i32  {
 
   // Create a new slice to track max x values
 
-  let mut v: Vec<i32> = vec![-1; 2 * max + 1];
-  let pos = v.as_mut_slice();
-  println!("pos length {}", pos.len());
-  pos[1] = 0;
+  let mut pos = CircularArray::new(2 * max + 1);
 
   // *** iterate over depth and k value ***
 
@@ -78,10 +53,10 @@ fn lcs(a: &str, b: &str) -> i32  {
   for d in 0..=max as i32 {
     for k in (-1 * d..=d).step_by(2) {
       let mut x: i32;
-      if k == (-1 * d) || (k != d && get_value(pos, k - 1) < get_value(pos, k + 1)) {
-        x = get_value(pos, k + 1);
+      if k == (-1 * d) || (k != d && pos.get(k - 1) < pos.get(k + 1)) {
+        x = pos.get(k + 1);
       } else {
-        x = get_value(pos, k - 1) + 1;
+        x = pos.get(k - 1) + 1;
       }
 
       let mut y = x - k;
@@ -92,7 +67,7 @@ fn lcs(a: &str, b: &str) -> i32  {
         y = y + 1;
       }
 
-      set_value(pos, k, x);
+      pos.set(k, x);
 
       if x >= n as i32 && y >= m as i32 {
         return d;
@@ -113,39 +88,5 @@ fn ses() -> i32 {
 pub fn run() {
   let _ = ses();
 
-  //test_circular_array();
-
-  //let _ = lcs("hello", "there");
   println!("shortest edit distance: {}", lcs("hello", "there"));
-
-  //test_circ_array();
 }
-
-/*
-fn test_circular_array() {
-  let mut v = vec![-1;5];
-  let p = v.as_mut_slice();
-  println!("{:?}", p);
-
-  set_value(p, -3, 5);
-  println!("{:?}", p);
-  
-  let value = get_value(p, -3);
-  println!("value at index -3 is {}", value);
-}
-*/
-
-/*
-fn test_circ_array() {
-  let mut circ = CircArray::new(5);
-  println!("circ: {:?}", circ.arr);
-  circ.arr[0] = 5;
-  println!("circ: {:?}", circ.arr);
-
-  let mut circ2 = CircArray::new(5);
-  println!("circ2: {:?}", circ2.arr);
-  circ2.set(-2, 5);
-  println!("circ2: {:?}", circ2.arr);
-  println!("value at index -2 in circ2 {:?}", circ2.get(-2));
-}
-*/
