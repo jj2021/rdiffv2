a417846323d15fad3c581d83f86c7a8ac82b73e7
jj2021 <james.jenkins2@marist.edu>
2022-11-05 19:26:59 +0000

    Read command line arguments
    Pass file paths as cli arguments and read file contents.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
-use rdiff::run; // creates "named import" of the run function from the rdiff.rs module
 mod rdiff; // includes code from rdiff.rs 
+use rdiff::run; // creates "named import" of the run function from the rdiff.rs module
+use std::fs;
+use std::env;
 
 fn main() {
+
+  let args: Vec<String> = env::args().collect();
+
+  if args.len() < 2 {
+    println!("rdiff requires two file paths as arguments\n");
+    return
+  }
+
+  let f1 = fs::read_to_string(&args[1]).expect("Could not read file");
+  println!("file1 content:\n{}", f1);
+
+  let f2 = fs::read_to_string(&args[2]).expect("Could not read file");
+  println!("file2 content:\n{}", f2);
+
   run();
 }
