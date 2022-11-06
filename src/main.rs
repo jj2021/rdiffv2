82e02c1a2e468d767107a54fc236be045d153b16
jj2021 <james.jenkins2@marist.edu>
2022-11-06 00:02:14 +0000

    Count lines in file
    Count lines in file to make a collection of lines.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod rdiff; // includes code from rdiff.rs 
 use rdiff::run; // creates "named import" of the run function from the rdiff.rs module
 use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::env;
 
 fn main() {
@@ -14,6 +16,14 @@ fn main() {
 
   let f1 = fs::read_to_string(&args[1]).expect("Could not read file");
   let f2 = fs::read_to_string(&args[2]).expect("Could not read file");
+
+  // Place lines of file into a vec
+  if let Ok(f) = File::open(&args[1]) {
+    let reader = io::BufReader::new(f);
+    let num_lines = reader.lines().count();
+    println!("file has {} lines\n", num_lines);
+  }
+
 
   run(&f1, &f2);
 }
