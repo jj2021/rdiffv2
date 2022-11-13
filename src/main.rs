5b0fa2df8effc26467152ea86b914da2ebd6737b
jj2021 <james.jenkins2@marist.edu>
2022-11-13 03:17:12 +0000

    Remove string diff
    Remove logic that finds the diff of two strings.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod rdiff; // includes code from rdiff.rs 
 use rdiff::run; // creates "named import" of the run function from the rdiff.rs module
-use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::env;
@@ -14,25 +13,21 @@ fn main() {
     return
   }
 
-  // convert files to strings
-  let s1 = fs::read_to_string(&args[1]).expect("Could not read file");
-  let s2 = fs::read_to_string(&args[2]).expect("Could not read file");
-
-  // diff strings
-  run(&s1, &s2);
-
   // Place lines of file into a vec
-  let f3 = file_to_vec(&args[1]);
-  let f4 = file_to_vec(&args[2]);
+  let f1 = file_to_vec(&args[1]);
+  let f2 = file_to_vec(&args[2]);
+
+  println!("--- {}", &args[1]);
+  println!("+++ {}", &args[2]);
 
   // diff files
-  rdiff::run_on_files(f3, f4);
+  run(f1, f2);
 }
 
+// convert file to vec of lines
 fn file_to_vec(file_name: &str) -> Vec<String> {
   let f = File::open(file_name).unwrap();
   let reader = io::BufReader::new(f);
   let collect_lines: Vec<String> = reader.lines().map(|x| x.unwrap()).collect();
-  println!("vec file lines: {:?}\n", collect_lines);
   collect_lines
 }
--- src/rdiff.rs
+++ src/rdiff.rs
@@ -30,142 +30,17 @@ impl CircularArray {
   }
 }
 
-// lcs produces the shortest edit sequence graph, given two strings
-fn lcs(a: &str, b: &str) -> Vec<CircularArray> {
+pub fn run(first_text: Vec<String>, second_text: Vec<String>) {
+  let mut edit_trace = lcs(&first_text, &second_text);
+  ses(&mut edit_trace, &first_text, &second_text);
+}
+
+// lcs produces the shortest edit sequence graph, given two files 
+fn lcs(a: &Vec<String>, b: &Vec<String>) -> Vec<CircularArray> {
   // *** SETUP ***
-  let n = a.chars().count(); // len() returns num bytes, not num characters
-  let m = b.chars().count();
-  println!("a is {} char long and b is {} char long", n, m);
-  let max = n + m;
-  println!("the max edits is {}", max);
-
-  // k: (x - y)
-  // d: depth (iteration)
-  // pos: track x value for each k (circular array)
-
-  // Create a new slice to track max x values
-
-  let mut pos = CircularArray::new(2 * max + 1);
-  pos.set(1, 0);
-
-  // *** iterate over depth and k value ***
-
-  // Track values for all iterations => the full diff graph
-  let mut trace: Vec<CircularArray> = vec![];
-
-  // max is of type usize, so we have to cast to i32 to allow k to be negative
-  // maybe try using isize type instead of i32???
-  for d in 0..=max as i32 {
-    for k in (-1 * d..=d).step_by(2) {
-      let mut x: i32;
-      if k == (-1 * d) || (k != d && pos.get(k - 1) < pos.get(k + 1)) {
-        x = pos.get(k + 1);
-      } else {
-        x = pos.get(k - 1) + 1;
-      }
-
-      let mut y = x - k;
-
-      // find diagonal moves
-      while x < n as i32 && y < m as i32 && a.chars().nth(x as usize) == b.chars().nth(y as usize) {
-        x = x + 1;
-        y = y + 1;
-      }
-
-      pos.set(k, x);
-
-      if x >= n as i32 && y >= m as i32 {
-        return trace;
-      }
-
-    }
-    trace.push(pos.clone());
-  }
-  // returns -1 if there is an error
-  //-1 
-  vec![]
-}
-
-// ses backtracks through a given diff graph to display the edit sequence
-fn ses(trace: &mut Vec<CircularArray>, a: &str, b: &str) {
-  let mut diff: Vec<String> = vec![];
-
-  let mut x = a.chars().count() as i32;
-  let mut y = b.chars().count() as i32;
-
-  let mut d = trace.len() as i32;
-  let mut d_pos = trace.pop(); // pop returns an Option<CircularArray> => Needs to be unwrapped to get underlying value
-
-  while d_pos.is_some() {
-    let pos = d_pos.unwrap();
-    let k = x - y;
-
-    let prev_k: i32;
-    if k == (-1 * d) || (k != d && pos.get(k - 1) < pos.get(k + 1)) {
-      prev_k = k + 1;
-    } else {
-      prev_k = k - 1;
-    }
-
-    let prev_x = pos.get(prev_k);
-    let prev_y = prev_x - prev_k;
-
-    // Trace diagonals in path if they exist
-    while x > prev_x && y > prev_y {
-      diff.push(format!("  {}", &a[(x - 1) as usize..x as usize]));
-      x = x - 1; 
-      y = y - 1;
-    }
-
-    if d > 0 {
-      if x == prev_x {
-        diff.push(format!("+ {}", &b[(y - 1) as usize..y as usize]));
-      } else if y == prev_y {
-        diff.push(format!("- {}", &a[(x - 1) as usize..x as usize]));
-      } else {
-        diff.push(format!("  {}", &a[(x - 1) as usize..x as usize]));
-      }
-    }
-
-    x = prev_x;
-    y = prev_y;
-
-    // Checkout x values for the previous depth iteration
-    d = trace.len() as i32;
-    d_pos = trace.pop();
-  }
-
-  // display the diff
-  let mut next_line = diff.pop();
-  while next_line.is_some() {
-    let line = next_line.unwrap();
-    println!("{}", line);
-    next_line = diff.pop();
-  }
-
-}
-
-// TODO: Utilize diff logic in a CLI applicaation
-pub fn run(first_text: &str, second_text: &str) {
-  let mut edit_trace = lcs(first_text, second_text);
-  println!("shortest edit distance: {}", edit_trace.len());
-  ses(&mut edit_trace, first_text, second_text);
-}
-
-pub fn run_on_files(first_text: Vec<String>, second_text: Vec<String>) {
-  let mut edit_trace = lcs_on_files(&first_text, &second_text);
-  println!("shortest edit distance: {}", edit_trace.len());
-  ses_on_files(&mut edit_trace, &first_text, &second_text);
-}
-
-// lcs produces the shortest edit sequence graph, given two strings
-fn lcs_on_files(a: &Vec<String>, b: &Vec<String>) -> Vec<CircularArray> {
-  // *** SETUP ***
-  let n = a.len(); // len() returns num bytes, not num characters
+  let n = a.len();
   let m = b.len();
-  println!("a is {} char long and b is {} char long", n, m);
   let max = n + m;
-  println!("the max edits is {}", max);
 
   // k: (x - y)
   // d: depth (iteration)
@@ -178,7 +53,7 @@ fn lcs_on_files(a: &Vec<String>, b: &Vec<String>) -> Vec<CircularArray> {
 
   // *** iterate over depth and k value ***
 
-  // Track values for all iterations => the full diff graph
+  // Track values for all iterations => the diff graph
   let mut trace: Vec<CircularArray> = vec![];
 
   // max is of type usize, so we have to cast to i32 to allow k to be negative
@@ -214,7 +89,8 @@ fn lcs_on_files(a: &Vec<String>, b: &Vec<String>) -> Vec<CircularArray> {
   vec![]
 }
 
-fn ses_on_files(trace: &mut Vec<CircularArray>, a: &Vec<String>, b: &Vec<String>) {
+// ses backtracks through a given diff graph to display the edit sequence
+fn ses(trace: &mut Vec<CircularArray>, a: &Vec<String>, b: &Vec<String>) {
   let mut diff: Vec<String> = vec![];
 
   let mut x = a.len() as i32;
